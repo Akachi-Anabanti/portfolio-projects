f6f9704e0661ac38d2165e874357967e008c8665
Akachi Anabanti <125439280+Akachi-Anabanti@users.noreply.github.com>
2023-08-11 00:58:26 +0100

    resume builder bublin format

---
+++ Resume-builder/assests/js/app.js
@@ -0,0 +1,368 @@
+const emailRegex =
+  /^(([^<>()\[\]\\.,;:\s@*]+(\.[^<>()\[\]\\.,:\s@*]+)*))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\-)+[a-zA-Z]{2,}))$/;
+const digitRegex = /^\d+$/;
+const strRegex = /^[a-zA-z\s]*$/;
+const PhoneRegex =
+  /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+const mainForm = document.getElementById("cv-form");
+const validType = {
+  TEXT: "text",
+  TEXT_EMP: "text_emp",
+  EMAIL: "email",
+  DIGIT: "digit",
+  PHONENO: "phoneno",
+  ANY: "any",
+};
+// user input elements
+let firstnameElem = mainForm.firstname,
+  middlenameElem = mainForm.middlename,
+  lastnameElem = mainForm.lastname,
+  imageElem = mainForm.image,
+  designationElem = mainForm.designation,
+  addressElem = mainForm.address,
+  emailElem = mainForm.email,
+  phonenoElem = mainForm.phoneno,
+  summaryElem = mainForm.summary;
+
+//-------------Display elements----------
+let nameDsp = document.getElementById("fullname_dsp"),
+  imageDsp = document.getElementById("image_dsp"),
+  phonenoDsp = document.getElementById("phoneno_dsp"),
+  emailDsp = document.getElementById("email_dsp"),
+  addressDsp = document.getElementById("address_dsp"),
+  designationDsp = document.getElementById("designation_dsp"),
+  summaryDsp = document.getElementById("summary_dsp"),
+  projectsDsp = document.getElementById("projects_dsp"),
+  achievementsDsp = document.getElementById("achievements_dsp"),
+  skillsDsp = document.getElementById("skills_dsp"),
+  educationsDsp = document.getElementById("educations_dsp"),
+  experiencesDsp = document.getElementById("experiences_dsp");
+
+//first value is the attributes and second one passes the nodelist
+const fetchValues = (attrs, ...nodeList) => {
+  let elemAttrsCount = nodeList.length,
+    elemsDataCount = nodeList[0].length,
+    tempDataArr = [];
+
+  //no of repeaters value
+  for (let i = 0; i < elemsDataCount; i++) {
+    let datObj = {}; //empty list object to fill the data
+
+    //data from the attributes
+    for (let j = 0; j < elemAttrsCount; j++) {
+      datObj[`${attrs[j]}`] = nodeList[j][i].value;
+    }
+    tempDataArr.push(datObj);
+  }
+  return tempDataArr;
+};
+
+const getUserInputs = () => {
+  //ACHIEVEMENTS
+  let achievementsTitleElem = document.querySelectorAll(".achieve_title"),
+    achievementsDescriptionElem = document.querySelectorAll(
+      ".achieve_description"
+    );
+
+  //EXPERIENCE
+  let expTitleElem = document.querySelectorAll(".exp_title"),
+    expOrganizationElem = document.querySelectorAll(".exp_organization"),
+    expStartDateElem = document.querySelectorAll(".exp_start_date"),
+    expEndDateElem = document.querySelectorAll(".exp_end_date"),
+    expDescriptionElem = document.querySelectorAll(".exp_description"),
+    expLocationElem = document.querySelectorAll(".exp_location");
+
+  //Education
+  let eduSchoolElem = document.querySelectorAll(".edu_school"),
+    eduDegreeElem = document.querySelectorAll(".edu_degree"),
+    eduCityElem = document.querySelectorAll(".edu_city"),
+    eduDescription = document.querySelectorAll(".edu_description"),
+    eduStartDateElem = document.querySelectorAll(".edu_start_date"),
+    eduEndDateElem = document.querySelectorAll(".edu_end_date");
+
+  //Projects
+  let projectTitleElem = document.querySelectorAll(".proj_title"),
+    projectLinkElem = document.querySelectorAll(".proj_link"),
+    projectDescriptionElem = document.querySelectorAll(".proj_description");
+
+  //Skills
+  let skillElem = document.querySelectorAll(".skill");
+
+  //-------------------------------------------------------------
+  //Eevent listener for formValidation
+  firstnameElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.TEXT, "First Name")
+  );
+  middlenameElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.TEXT_EMP, "Middle Name")
+  );
+  lastnameElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.TEXT, "Last Name")
+  );
+  phonenoElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.PHONENO, "Phone Number")
+  );
+  emailElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.EMAIL, "Email")
+  );
+  addressElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.ANY, "Address")
+  );
+  designationElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.TEXT, "Designation")
+  );
+  summaryElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.TEXT, "Summary")
+  );
+  achievementsTitleElem.forEach((item) =>
+    item.addEventListener("keyup", (e) =>
+      validateFormData(e.target, validType.ANY, "Title")
+    )
+  );
+
+  achievementsTitleElem.forEach((item) =>
+    item.addEventListener("keyup", (e) =>
+      validateFormData(e.target, validType.ANY, "Description")
+    )
+  );
+  expTitleElem.forEach((item) =>
+    item.addEventListener("keyup", (e) =>
+      validateFormData(e.target, validType.ANY, "Title")
+    )
+  );
+
+  expOrganizationElem.forEach((item) =>
+    item.addEventListener("keyup", (e) =>
+      validateFormData(e.target, validType.ANY, "Organization")
+    )
+  );
+  expLocationElem.forEach((item) =>
+    item.addEventListener("keyup", (e) =>
+      validateFormData(e.target, validType.ANY, "Location")
+    )
+  );
+  expDescriptionElem.forEach((item) =>
+    item.addEventListener("keyup", (e) =>
+      validateFormData(e.target, validType.ANY, "Description")
+    )
+  );
+  expStartDateElem.forEach((item) =>
+    item.addEventListener("blur", (e) =>
+      validateFormData(e.target, validType.ANY, "Start Date")
+    )
+  );
+  expEndDateElem.forEach((item) =>
+    item.addEventListener("blur", (e) =>
+      validateFormData(e.target, validType.ANY, "End Date")
+    )
+  );
+  eduSchoolElem.forEach((item) =>
+    item.addEventListener("keyup", (e) =>
+      validateFormData(e.target, validType.ANY, "School")
+    )
+  );
+  eduCityElem.forEach((item) =>
+    item.addEventListener("keyup", (e) =>
+      validateFormData(e.target, validType.ANY, "City")
+    )
+  );
+  eduDegreeElem.forEach((item) =>
+    item.addEventListener("keyup", (e) =>
+      validateFormData(e.target, validType.ANY, "Degree")
+    )
+  );
+  eduDescription.forEach((item) =>
+    item.addEventListener("keyup", (e) =>
+      validateFormData(e.target, validType.ANY, "Description")
+    )
+  );
+  eduStartDateElem.forEach((item) =>
+    item.addEventListener("blur", (e) =>
+      validateFormData(e.target, validType.ANY, "Start Date")
+    )
+  );
+  eduEndDateElem.forEach((item) =>
+    item.addEventListener("blur", (e) =>
+      validateFormData(e.target, validType.ANY, "End Date")
+    )
+  );
+  projectTitleElem.forEach((item) =>
+    item.addEventListener("keyup", (e) =>
+      validateFormData(e.target, validType.ANY, "Title")
+    )
+  );
+  projectLinkElem.forEach((item) =>
+    item.addEventListener("keyup", (e) =>
+      validateFormData(e.target, validType.ANY, "Link")
+    )
+  );
+  projectDescriptionElem.forEach((item) =>
+    item.addEventListener("keyup", (e) =>
+      validateFormData(e.target, validType.ANY, "Description")
+    )
+  );
+  skillElem.forEach((item) =>
+    item.addEventListener("keyup", (e) =>
+      validateFormData(e.target, validType.ANY, "Skill")
+    )
+  );
+  //-------------------------------------------------------------------
+  ///RETURN STATEMENT
+  return {
+    firstname: firstnameElem.value,
+    middlename: middlenameElem.value,
+    lastname: lastnameElem.value,
+    designation: designationElem.value,
+    address: addressElem.value,
+    email: emailElem.value,
+    phoneno: phonenoElem.value,
+    summary: summaryElem.value,
+    //achievements
+    achievements: fetchValues(
+      ["achieve_title", "achieve_description"],
+      achievementsTitleElem,
+      achievementsDescriptionElem
+    ),
+    //experiences
+    experiences: fetchValues(
+      [
+        "exp_title",
+        "exp_organization",
+        "exp_location",
+        "exp_start_date",
+        "exp_end_date",
+        "exp_description",
+      ],
+      expTitleElem,
+      expOrganizationElem,
+      expLocationElem,
+      expStartDateElem,
+      expEndDateElem,
+      expDescriptionElem
+    ),
+    //EDUCATION
+    educations: fetchValues(
+      [
+        "edu_school",
+        "edu_degree",
+        "edu_city",
+        "edu_description",
+        "edu_start_date",
+        "edu_end_date",
+      ],
+      eduSchoolElem,
+      eduDegreeElem,
+      eduCityElem,
+      eduDescription,
+      eduStartDateElem,
+      eduEndDateElem
+    ),
+    //PROJECT
+    projects: fetchValues(
+      ["proj_title", "proj_link", "proj_description"],
+      projectTitleElem,
+      projectLinkElem,
+      projectDescriptionElem
+    ),
+    //skill
+    skills: fetchValues(["skill"], skillElem),
+  };
+};
+
+/// FORM VALIDATION
+/**
+ *
+ * @param {*} elem
+ * @param {*} elemType
+ * @param {*} elemName
+ */
+function validateFormData(elem, elemType, elemName) {
+  if (elemType == validType.TEXT) {
+    if (!strRegex.test(elem.value) || elem.value.trim().length == 0)
+      addErrMsg(elem, elemName);
+    else removeErrMsg(elem);
+  }
+  if (elemType == validType.TEXT_EMP) {
+    if (!strRegex.test(elem.value)) addErrMsg(elem, elemName);
+    else removeErrMsg(elem);
+  }
+  if (elemType == validType.EMAIL) {
+    if (!emailRegex.test(elem.value) || elem.value.trim().length == 0)
+      addErrMsg(elem, elemName);
+    else removeErrMsg(elem);
+  }
+
+  if (elemType == validType.PHONENO) {
+    if (!PhoneRegex.test(elem.value) || elem.value.trim().length == 0)
+      addErrMsg(elem, elemName);
+    else removeErrMsg(elem);
+  }
+  if (elemType == validType.ANY) {
+    if (elem.value.trim().length == 0) addErrMsg(elem, elemName);
+    else removeErrMsg(elem);
+  }
+}
+
+//Add error message
+function addErrMsg(formElem, formElemName) {
+  formElem.nextElementSibling.innerHTML = `${formElemName} is invalid`;
+}
+
+//remove the error message
+function removeErrMsg(formElem) {
+  formElem.nextElementSibling.innerHTML = "";
+}
+
+// show the list data
+const showListData = (listData, listContainer) => {
+  listContainer.innerHTML = "";
+  listData.forEach((listItem) => {
+    let itmeElem = document.createElement("div");
+    itmeElem.classList.add("preview-item");
+
+    for (const key in listItem) {
+      let subItemElem = document.createElement("span");
+      subItemElem.classList.add("preview-item-val");
+      subItemElem.innerHTML = `${listItem[key]}`;
+      itmeElem.appendChild(subItemElem);
+    }
+    listContainer.appendChild(itmeElem);
+  });
+};
+
+const displayCV = (userData) => {
+  nameDsp.innerHTML =
+    userData.firstname + " " + userData.middlename + " " + userData.lastname;
+  phonenoDsp.innerHTML = userData.phoneno;
+  emailDsp.innerHTML = userData.email;
+  addressDsp.innerHTML = userData.address;
+  designationDsp.innerHTML = userData.designation;
+  summaryDsp.innerHTML = userData.summary;
+  showListData(userData.projects, projectsDsp);
+  showListData(userData.achievements, achievementsDsp);
+  showListData(userData.skills, skillsDsp);
+  showListData(userData.educations, educationsDsp);
+  showListData(userData.experiences, experiencesDsp);
+};
+
+/**
+ *
+ */
+const generateCV = () => {
+  let userData = getUserInputs();
+  displayCV(userData);
+};
+
+function previewImage() {
+  let ofReader = new FileReader();
+  ofReader.readAsDataURL(imageElem.files[0]);
+  ofReader.onload = function (ofEvent) {
+    imageDsp.src = ofEvent.target.result;
+  };
+}
+
+//Print CV
+function printCV() {
+  window.print();
+}
